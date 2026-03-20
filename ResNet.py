931d520cb044dd01a6678c373e637eafea273f13
birdf00t <koeunbi489@gmail.com>
2026-03-20 14:00:18 +0900

    change nn module

--- ResNet.py
+++ ResNet.py
@@ -1,16 +1,21 @@
 import torch
 import torch.nn as nn
 from residualBlock import residualBlock
+
+
 class ResNet(nn.Module):
     def __init__(self, n=3, num_classes = 10):
         super().__init__()
-        self.layer1 = nn.ModuleList([residualBlock(in_ch = 3, out_ch=16)] + [residualBlock(16,16) for _ in range(n-1)])
+        self.conv1 = nn.Conv2d(3,16,kernel_size=3,stride=1,padding=1,bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = nn.ModuleList([residualBlock(16,16) for _ in range(n)])
         self.layer2= nn.ModuleList([residualBlock(in_ch = 16 , out_ch=32,stride=2)]+ [residualBlock(32,32) for _ in range(n-1)])
         self.layer3= nn.ModuleList([residualBlock(in_ch = 32, out_ch=64,stride=2)]+ [residualBlock(64,64) for _ in range(n-1)])
         self.fc = torch.nn.Linear(64,num_classes)
 
     def forward(self,x):
         out = x
+        out = torch.relu(self.bn1(self.conv1(x)))
         for block in self.layer1:
             out = block.forward(out)
         for block in self.layer2:
--- main.py
+++ main.py
@@ -5,14 +5,18 @@ import data_func
 from ResNet import ResNet
 
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
 #데이터 없으면 다운로드 있으면 불러오기
 trainloader, testloader = data_func.get_dataloaders()
 
-model = ResNet()
+model = ResNet().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(),lr=0.1,momentum=0.9)
 for epoch in range(10):
     for images, labels in trainloader:
+        images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
         output = model.forward(images)
         loss = criterion(output,labels)
@@ -25,6 +29,7 @@ correct = 0
 total = 0
 with torch.no_grad():
     for images, labels in testloader:
+        images, labels = images.to(device), labels.to(device)
         output = model.forward(images)
         _, predicted = torch.max(output, 1)
         total += labels.size(0)
